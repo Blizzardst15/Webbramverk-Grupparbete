920fbedfe64a4adbf2f326cd8e21018803ba7cc8
Sara Razm <razmsara@gmail.com>
2022-03-14 14:05:47 +0100

    importing pages in app.js and fixing css for the header

--- jensen/src/App.js
+++ jensen/src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sara from "./components/pages/Sara";
 import NoPage from "./components/pages/NoPage";
+import Bomi from "./components/pages/Bomi";
+import Erik from "./components/pages/Erik";
+import Kristofer from "./components/pages/Kristofer";
+import Header from "./components/Header";
+import Footer from "./components/footer/Footer"
 
 
 function App() {
