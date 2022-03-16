b343f49ef29f62ab4cd0113d4d0217ba9a6f6221
Båmi Lee <swedenebomi@gmail.com>
2022-03-17 00:49:31 +0100

    component test

--- jensen/src/App.js
+++ jensen/src/App.js
@@ -1,16 +1,4 @@
-// // import React,{Component} from 'react';
-// import './App.css';
-// import Student from './components/Student'
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <Student/>
-//     </div>
-//   );
-// }
-
-// export default App;
 
 import Student from './components/Student'
 import './App.css';
