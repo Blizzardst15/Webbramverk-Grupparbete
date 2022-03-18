341593c372656d5e67b13975194fb45dc64c5c10
Båmi Lee <swedenebomi@gmail.com>
2022-03-18 10:36:44 +0100

    info

--- jensen/src/components/pages/Bomi/Info.js
+++ jensen/src/components/pages/Bomi/Info.js
@@ -1,20 +1,14 @@
-import React from 'react';
+import React from "react";
 
-
-function Info() {
-return (
-    class Student extends React.Component {
-    
-        return(){
-            <div>
-                <h2>Rapunzel </h2>
-                <h2>Picasso  </h2>
-                <p>rapunzel@email.com</p>
-                <p>Webbramverk för Javascript</p>
-            </div>
-        }
-    }
-);
+const Info = () => {
+  return (
+    <div>
+      <h2>Rapunzel </h2>
+      <h2>Picasso </h2>
+      <p>rapunzel@email.com</p>
+      <p>Webbramverk för Javascript</p>
+    </div>
+  );
 }
 
 export default Info;
