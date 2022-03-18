e08c835dc4a5a5d8092995ca368652cb74890f98
Båmi Lee <swedenebomi@gmail.com>
2022-03-18 14:44:29 +0100

    utbilding info1 done

--- jensen/src/components/pages/Bomi/Info.js
+++ jensen/src/components/pages/Bomi/Info.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React,{useState} from 'react';
+import './Info.css';
+import UtbList from './UtbList/UtbList';
+import NewUtb from './NewUtb/NewUtb';
 
-const Info = () => {
-  return (
-    <div>
-      <h2>Rapunzel </h2>
-      <h2>Picasso </h2>
-      <p>rapunzel@email.com</p>
-      <p>Webbramverk för Javascript</p>
+
+const Info = ()=>{
+  const [utbNames, setUtbNames]= useState([
+
+    {id: 'utb1', text: 'Frontendutvecklare inriktning webbsäkerhet'},
+    {id: 'utb1', text: 'AI Developer'},
+    {id: 'utb1', text: 'Cloud Amazon webbservice'},
+    {id: 'utb1', text: 'Teknisk testare'},
+  ]);
+
+  const addNewUtbHandler = newUtb =>{
+    setUtbNames((preUtbNames)=>{
+      return preUtbNames.concat(newUtb);
+    });
+    // setUtbNames(utbNames.concat(newUtb));
+    // utbNames.push(newUtb);
+    // console.log(utbNames);
+  };
+
+  
+  return(
+    <div className="kurs-app">
+      <h1 title="Jensen YH">Välkommen,<span> Ansökan</span> till Jensens utbildiningar!</h1>
+      <h2>utbildiningar Info</h2>
+      <h3>(Ny utbildning: IT 💻 & music ♩♪♫♬♭♮♯ & eating broadcasting 🍣🍳🍔🍟!)</h3>
+      <NewUtb  onAddUtb ={addNewUtbHandler}/>
+      <UtbList utbs ={utbNames}/>
     </div>
-  );
+  )
 }
 
+
+
+
+
+
 export default Info;
+
+
