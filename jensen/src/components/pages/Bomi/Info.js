ecb75a344d122c3b70a073ed6cc0fccdc432e2d4
Båmi Lee <swedenebomi@gmail.com>
2022-03-21 10:40:25 +0100

    frontend del commit

--- jensen/src/components/pages/Bomi/Info.js
+++ jensen/src/components/pages/Bomi/Info.js
@@ -1,17 +1,33 @@
 
-import React, {useState} from "react";
+
+//by using the find index method so we¨will say cost index...
+//pass in arrow function which get the current contact as a parameter
+
+import React, {useState, Fragment} from "react";
+import {nanoid} from 'nanoid';
 import "./Info.css";
 import data from "./mock-data.json"
-
+import ReadOnlyRow from './components/ReadOnlyRow';
+import EditableRow from './components/EditableRow';
 
 const Info =() =>{
+
   const[contacts, setContacts] = useState(data);  
   const[addFormData, setAddFormData] = useState({
-    firstName:'',
-    lastName:'',
-    email:'',
-    major: ''
+    firstName:"",
+    lastName:"",
+    email:"",
+    major: "",
   });
+
+  const [editFormData, setEditFormData] = useState({
+    firstName:"",
+    lastName:"",
+    email:"",
+    major:"",
+  })
+
+  const [editContactId, setEditContactId]= useState(null);
 
   const handleAddFormChange = (event)=>{
     event.preventDefault();
@@ -23,8 +39,85 @@ const Info =() =>{
   newFormData[fieldName]= fieldValue;
 
   setAddFormData(newFormData);
+  };
+
+  const handleEditFormChange = (event)=>{
+    event.preventDefault();
+
+    const fieldName = event.target.getAttribute("name");
+    const fieldValue = event.target.value;
+
+    const newFormData = {...editFormData};
+    newFormData[fieldName] = fieldValue;
+
+    setEditFormData(newFormData);
   }
+
+  const handleAddFormSubmit = (event) =>{
+    event.preventDefault();
+
+    const newContact = {
+      id: nanoid(),
+      firstName: addFormData.firstName,
+      lastName: addFormData.lastName,
+      major: addFormData.major,
+      email: addFormData.email
+    }
+
+    const newContacts = [...contacts, newContact]
+    setContacts(newContacts);
+  };
+
+  const handleEditFormSubmit = (event) =>{
+    event.preventDefault();
+
+    const editedContact = {
+      id: editContactId,
+      firstName: editFormData.firstName,
+      lastName: editFormData.lastName,
+      major: editFormData.major,
+      email: editFormData.email
+    }
+    const newContacts =[...contacts];
+    const index = contacts.findIndex((contact)=> contact.id === editContactId)
+    newContacts[index] = editedContact;
+    setContacts(newContacts);
+    setEditContactId(null);
+  };
+
+  const handleEditClick = (event, contact)=>{
+    event.preventDefault();
+    setEditContactId(contact.id);
+
+  const formValues = {
+    firstName: contact.firstName,
+    lastName: contact.lastName,
+    major: contact.major,
+    email: contact.email,
+  }
+  setEditFormData(formValues);
+  };
+
+  const handleCancelClick = () => {
+    setEditContactId(null);
+  }
+
+  const handleDeleteClick = ()=>{
+    const newContacts = [...contacts];
+    //next we will use the splice method to remove contact object
+    //at the given index in the array.
+
+    const index = contacts.findIndex((contact)=> contact.id === editContactId);
+    newContacts.splice(index, 1);
+    setContacts(newContacts);
+    
+
+
+  }
+
   return (<div className="app-container">
+    <h1 style={{marginTop:"30px",textAlign:"center"}}>Ansökan till Jensen YH </h1>
+  <form onSubmit={handleEditFormSubmit}>
   <table>
     <thead>
       <tr>
@@ -32,22 +125,37 @@ const Info =() =>{
         <th>LastName </th>
         <th>Email</th>
         <th>Major </th>
+        <th>Actions</th>
       </tr>
     </thead>
     <tbody>
       {contacts.map((contact)=> (
-          <tr>
-          <td>{contact.firstName}</td>
-          <td>{contact.lastName}</td>
-          <td>{contact.email}</td>
-          <td>{contact.major}</td>
-          </tr>
+          <Fragment>
+            {/* <form> */}
+          {/* <EditableRow/> */}
+            {/* </form> */}
+            {editContactId === contact.id ? (
+            <EditableRow 
+            editFormData={editFormData}
+            handleEditFormChange={handleEditFormChange}
+            handleCancelClick={handleCancelClick}
+            />
+            ) : (
+              <ReadOnlyRow 
+              contact={contact} 
+              handleEditClick={handleEditClick}
+              handleDeleteClick={handleDeleteClick}
+              />
+            )}
+          
+          </Fragment>
       ))}
     </tbody>
   </table>
+  </form>
 
-  <h2>Add Ansökan här !</h2>
-  <form>
+  <h2>Add din favorit utbilding& info här !</h2>
+  <form onSubmit={handleAddFormSubmit}>
     <input 
     type="text" 
     name="firstName" 
@@ -83,6 +191,9 @@ const Info =() =>{
 };
 
 export default Info;
+
+
+
 
 
 
