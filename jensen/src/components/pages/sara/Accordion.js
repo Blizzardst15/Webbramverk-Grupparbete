9da8f4ced9a7d6766c16e265b2c7c7405c2e83b6
Sara Razm <sara.razm@peab.se>
2022-03-22 15:40:55 +0100

    setting the rotation of cherven icon

--- jensen/src/components/pages/sara/Accordion.js
+++ jensen/src/components/pages/sara/Accordion.js
@@ -7,10 +7,12 @@ const Accordion = (props) => {
     const [setActive, setActiveState] = useState("");
     const [setHeight, setHeightState] = useState("0px");
     const content = useRef(null);
+    const [setRotate, setRotateState] = useState("accordion_icon")
 
     function toggleAccordion() {
         setActiveState(setActive === "" ? "active" : "")
         setHeightState(setActive === "active" ? "0px" : `${content.current.scrollHeight}px`);
+        setRotateState(setActive === "active" ? "accordion_icon" : "accordion_icon rotate");
     }
     return (
         <div className="accordion_section">
@@ -19,7 +21,7 @@ const Accordion = (props) => {
                 <p className="accordion_title">
                     {props.title}
                 </p>
-                <Chevron width={10} fill={"#777"} />
+                <Chevron className={`${setRotate}`} width={10} fill={"#777"} />
             </button>
 
             <div ref={content} style={{ maxHeight: `${setHeight}` }} className="accordion_content">
