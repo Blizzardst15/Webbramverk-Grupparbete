fa6d9a20c28e54c6613bd5b464e583a59a07e371
Båmi Lee <swedenebomi@gmail.com>
2022-07-30 17:35:27 +0200

    need update fuction
    Signed-off-by: Båmi Lee <swedenebomi@gmail.com>

--- jensen/src/components/pages/Bomi/Ansokan.js
+++ jensen/src/components/pages/Bomi/Ansokan.js
@@ -1,55 +1,17 @@
-import React, {useState, useEffect} from 'react';
+const Ansokan = ({id, descrition, title, fullName, email, major, onDelete}) =>{
+    return(
+        <div>
+            <h3>{title}</h3>
+            <p>{fullName}</p>
+            <p>{email}</p>
+            <p>{major}</p>
+            <p>{`ID: ${id}`}</p>
+            <button onClick={()=> onDelete(id)}>Delete</button>
 
 
+        </div>
 
-
-
-const Ansokan = () => {
-    description,
-    title,
-    fullName,
-    email,
-    major,
-    onDelete,
-    id, updateAnsokan,
-    updateDescription,
-    updateTitle,
-    updateFullName,
-    updateEmail,
-    updateMajor,
-    editTitle,
-    editDescription,
-    editFullName,
-    editEmail,
-    editMajor 
-
-
-return (
-    <div>
-    <input value={updateFullName} onChange={(e) => {
-                        editFullName(e.target.value)
-                        console.log(updateFullName)
-                    }} type="text" className="edit" placeholder="Full name" />
-    <input value={updateDescription} onChange={(e) => {
-                        editDescription(e.target.value)
-                        console.log(updateDescription)
-                    }} type="text" className="edit" placeholder="Description" />
-    <input value={updateTitle} onChange={(e) => {
-                        editTitle(e.target.value)
-                        console.log(updateTitle)
-                    }} type="text" className="edit" placeholder="Title" />
-
-    <input value={updateEmail} onChange={(e) => {
-                        editTitle(e.target.value)
-                        console.log(updaEmail)
-                    }} type="text" className="edit" placeholder="Email" />
-
-    <input value={updateMajor} onChange={(e) => {
-                        editTitle(e.target.value)
-                        console.log(updateMajor)
-                    }} type="text" className="edit" placeholder="Major" />
-    </div>
-)
+    )
 }
 
-export default Ansokan
+export default Ansokan;
